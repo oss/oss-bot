2ff0ff1076865cf8d10be157c7268a552e582006
James Chambers <jameschambers2@gmail.com>
2016-05-06 15:22:56 -0400

    Add GitHub event notifier

--- bot_config.sample.sh
+++ bot_config.sample.sh
@@ -1,3 +1,14 @@
+OSS_ROOM="oss@conference.rutgers.edu"
+OSS_ROOM_PASS="password"
+
+# XMPP Credentials
 export HUBOT_XMPP_USERNAME="hubot@rutgers.edu"
 export HUBOT_XMPP_PASSWORD="password"
-export HUBOT_XMPP_ROOMS="oss@conference.rutgers.edu:password"
+export HUBOT_XMPP_ROOMS="$OSS_ROOM:$OSS_ROOM_PASS"
+
+# Webhook config
+export PORT="8081"
+
+# GitHub notifier
+export HUBOT_GITHUB_EVENT_NOTIFIER_ROOM="$OSS_ROOM"
+export HUBOT_GITHUB_EVENT_NOTIFIER_TYPES="ping"
