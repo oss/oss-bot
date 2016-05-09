9a4461c1ee3708abcb9800980b5cb902e1c26cf5
James Chambers <jameschambers2@gmail.com>
2016-05-09 16:27:41 -0400

    Update default github notifier events

--- bot_config.sample.sh
+++ bot_config.sample.sh
@@ -11,4 +11,7 @@ export PORT="8081"
 
 # GitHub notifier
 export HUBOT_GITHUB_EVENT_NOTIFIER_ROOM="$OSS_ROOM"
-export HUBOT_GITHUB_EVENT_NOTIFIER_TYPES="ping"
+export HUBOT_GITHUB_EVENT_NOTIFIER_TYPES="ping,commit_comment,create,delete,\
+deployment,deployment_status,fork,gollum,issue_comment,issues,member,membership,\
+page_build,public,pull_request_review_comment,pull_request,push,repository,release,\
+status,team_add,watch"
